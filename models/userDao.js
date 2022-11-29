20321f977e6b1a3d513943b62d5494518a38df0d
Seoya0512 <seoya.lee512@gmail.com>
2022-11-29 18:39:12 +0900

    [Revised] unit test 코드 작성

---
+++ models/userDao.js
@@ -0,0 +1,71 @@
+const { appDataSource } = require("./dataSource");
+
+const getUserByKakaoId = async (kakaoId) => {
+  const user = await appDataSource.query(
+    `SELECT
+            id, 
+            kakao_id,
+            first_name,
+            last_name,
+            birth,
+            mobile_number,
+            email   
+        FROM users
+        WHERE kakao_id =?`,
+    [kakaoId]
+  );
+  return user[0];
+};
+
+const storeKakaoId = async (kakaoId) => {
+  return await appDataSource.query(
+    `
+    INSERT INTO users (
+        kakao_id
+    ) VALUES (?);
+    `,
+    [kakaoId]
+  );
+};
+
+const updateUser = async (
+  userId,
+  first_name,
+  last_name,
+  birth,
+  mobile_number,
+  email
+) => {
+  await appDataSource.query(
+    `UPDATE users
+    SET first_name =?,last_name =?,birth=?,mobile_number= ?,email = ?
+    WHERE id = ?
+    `,
+    [first_name, last_name, birth, mobile_number, email, userId]
+  );
+};
+
+const getUserById = async (id) => {
+  const result = await appDataSource.query(
+    `
+		SELECT 
+      id,
+      first_name,
+      last_name,
+      birth,
+      mobile_number,
+      email
+    FROM users
+		WHERE id=?`,
+    [id]
+  );
+
+  return result[0];
+};
+
+module.exports = {
+  getUserByKakaoId,
+  storeKakaoId,
+  updateUser,
+  getUserById,
+};
--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,11 @@
 const express = require("express");
 const router = express.Router();
 
+const userRouter = require("./userRouter");
 const { bookingRouter } = require("./bookingRouter");
 
 router.use("/booking", bookingRouter);
+
+router.use("/users", userRouter);
 
 module.exports = router;
