2286389b21df41fb47b88e7a0e00b8c066cb2900
root <root@DESKTOP-B9G676P>
2022-12-02 13:44:06 +0900

    search_flight 완성

--- controllers/bookingController.js
+++ controllers/bookingController.js
@@ -1,8 +1,34 @@
 const bookingService = require("../services/bookingService");
+const { catchAsync, raiseCustomError } = require("../utils/error");
 
-const getcities = async (req, res) => {
-  const cities = await bookingService.getcities();
+const getCities = catchAsync(async (req, res) => {
+  const cities = await bookingService.getCities();
   return res.status(200).json({ countries: cities });
-};
+});
 
-module.exports = { getcities };
+const searchFlight = catchAsync(async (req, res) => {
+  const { date, departureId, arrivalId } = req.query;
+  const MINDEPARTUREID = 1;
+  const MAXDEPARTUREID = 11;
+
+  if (!date || !departureId || !arrivalId) {
+    raiseCustomError("No data", 400);
+  }
+  if (
+    departureId < MINDEPARTUREID ||
+    departureId > MAXDEPARTUREID ||
+    arrivalId < MINDEPARTUREID ||
+    arrivalId > MAXDEPARTUREID
+  ) {
+    raiseCustomError("invalid data", 400);
+  }
+
+  const flights = await bookingService.searchFlight(
+    date,
+    departureId,
+    arrivalId
+  );
+  return res.status(200).json({ tickets: flights });
+});
+
+module.exports = { getCities, searchFlight };
